f3dcd907802a7ec6afea90713bc3f6cdfa852157
karguello90 <karguello90@gmail.com>
2023-03-23 16:34:19 -0400

    updates to models

--- models/Thought.js
+++ models/Thought.js
@@ -1,25 +1,23 @@
 const { Schema, model } = require('mongoose');
-const assignmentSchema = require('./Reaction');
+const reactionsSchema = require('./Reaction');
 
-// Schema to create Student model
-const studentSchema = new Schema(
+// Schema to create User Model
+const thoughtSchema = new Schema(
   {
-    first: {
+    thoughtText: {
       type: String,
       required: true,
-      max_length: 50,
+      max_length: 280,
     },
-    last: {
+    username: {
       type: String,
       required: true,
-      max_length: 50,
     },
-    github: {
-      type: String,
-      required: true,
-      max_length: 50,
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
-    assignments: [assignmentSchema],
+    reactions: [reactionsSchema],
   },
   {
     toJSON: {
@@ -28,6 +26,6 @@ const studentSchema = new Schema(
   }
 );
 
-const Student = model('student', studentSchema);
+const Thought = model('thought', thoughtSchema);
 
-module.exports = Student;
+module.exports = Thought;
